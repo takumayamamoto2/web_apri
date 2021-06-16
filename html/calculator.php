a947b81ae03a49c44b6f343ef8373f076b574f24
Takuma Yamamoto <tak.ke-tai.mk2@ezweb.ne.jp>
2021-06-16 14:54:42 +0900

    計算する仕組み完成

--- html/calculator.php
+++ html/calculator.php
@@ -1,8 +1,14 @@
 <?php 
 
 require_once '../conf/const.php';
+require_once MODEL_PATH . 'function.php';
 
 
+$calcu_data = get_post('calculation');
 
+var_dump($calcu_data);
+
+$result_data = calculator($calcu_data);
+var_dump($result_data);
 
 include_once VIEW_PATH . 'calculator_view.php';
---
+++ html/calculator_result.php
@@ -0,0 +1,9 @@
+<?php 
+
+require_once '../conf/const.php';
+require_once MODEL_PATH . 'function.php';
+
+
+$calcu_data = get_post('calculation');
+
+
