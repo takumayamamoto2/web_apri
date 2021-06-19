618a586ebd7b1b637d5fab50ff4f05f7c51a0c9e
Takuma Yamamoto <tak.ke-tai.mk2@ezweb.ne.jp>
2021-06-19 15:50:55 +0900

    セキュリティ対策とその他javascript設定完了

--- conf/const.php
+++ conf/const.php
@@ -1,17 +1,5 @@
 <?php
 
-// データベースの初期設定
-define('DB_HOST', 'mysql');
-define('DB_NAME', 'material');
-define('DB_USER', 'metallic');
-define('DB_PASS', 'perfectcell2');
-define('DB_CHARSET', 'utf8');
-
-define('IMG_PATH', './assets/img/');
-define('IMG_DIR', $_SERVER['DOCUMENT_ROOT'] . '/assets/img/');
-define('BGM_PATH', './assets/bgm/');
-define('BGM_DIR', $_SERVER['DOCUMENT_ROOT'] . '/assets/bgm/');
-
 define('VIEW_PATH', $_SERVER['DOCUMENT_ROOT'] . '/../view/');
 define('MODEL_PATH', $_SERVER['DOCUMENT_ROOT'] . '/../model/');
 define('STYLESHEET_PATH', '/assets/css/');
@@ -19,14 +7,17 @@ define('STYLESHEET_PATH', '/assets/css/');
 define('HOME_URL', '/calculator.php');
 define('LOGIN_URL', '/login.php');
 
+define('COOKIE_TIME', 3600 * 24 * 365);
+define('TAX', 0.1);
 
-define('USER_NUMBER_ADMIN', 1);
-define('USER_NUMBER_NORMAL', 2);
-
-define('LOGIN_REGEX', '/^[a-zA-Z0-9]{6,}$/');
-define('INTEGER_REGEX', '/^[0-9]+$/');// 正規表現 半角数字
+define('FORMULA_REGEX', '/^[\-\+]?(\(?[\-\+]?([0-9]*)\.?([0-9]*)\)?[\-\+\*\/]?\)?\(?[\-\+]?([0-9]*))*$/'); // 正規表現
 
 define('DISPLAY_STATUS', array(
+    'close' => 0,
+    'open' => 1,
+));
+
+define('TAX_STATUS', array(
     'close' => 0,
     'open' => 1,
 ));
--- html/calculator.php
+++ html/calculator.php
@@ -3,13 +3,27 @@
 require_once '../conf/const.php';
 require_once MODEL_PATH . 'function.php';
 
+// セッション開始
+session_start();
+
 // 履歴表示の有無をクッキーから取得
 $display_mode = get_cookie('display');
 
+// 履歴表示の有無をクッキーから取得
+$tax_mode = get_cookie('tax');
+
 // 計算履歴の取得
 $result_historys = get_cookie('calcu_formula');
+// 配列の中身があるかどうかチェック
+if(is_array($result_historys) !== false){
+// 計算履歴をHTMLエンティティにして取得
+$result_historys = entity_str_array($result_historys);
+}
 
 // 計算結果の答えを取得
 $result_data = get_cookie('calcu_result');
+// 計算履歴をHTMLエンティティにして取得
+$result_data = entity_str($result_data);
+
 
 include_once VIEW_PATH . 'calculator_view.php';
